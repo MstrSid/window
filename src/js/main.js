c33948fa5821c81de0495520fea80be2dc2a5d5c
Eugene Kos <mstrsid@live.com>
2021-12-21 16:48:27 +0300

    done

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import './slider';
 import bindModal from './modules/modals';
 import {showModalByTimer} from './modules/modals';
+import tabs from './modules/tabs';
 
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -8,5 +9,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close', modalTimerId);
 	bindModal('.phone_link', '.popup_engineer', '.popup_engineer .popup_close', modalTimerId);
+
+	tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'slick-active');
+	tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
 
 });
